b4e392836b726a7b497d735c821d38cfd7e4b344
jisantuc <james.santucci@gmail.com>
2014-06-12 08:10:42 -0400

    including .py

---
+++ freelunch.py
@@ -0,0 +1,10 @@
+from sunlight import capitolwords
+from ggplot import *
+api_key = '887de3e17a5f445b879c20372af0a6a1'
+
+#need to get:
+#   method for counting anti-free-lunch-ist speeches given in congress by senator/rep
+#   database of congressional gifts received OR
+#   database of money spent dining lobbyists (free DINNER also counts, eh? ha)
+#   or both.
+#   plan for how to visualize (maybe d3py instead, if you feel like learning that; something like a McDouble that grows or shrinks based on how much money was spent or received on free lunches)
